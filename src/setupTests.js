a013a5ebc7a38ed47e686edd9c25da137e28879f
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-09-02 10:28:35 +0300

    Added boilerplate - tests, vitals

---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+ jest-dom adds custom jest matchers for asserting on DOM nodes.
+ allows you to do things like
+ expect(element).toHaveTextContent(reacti)
+ learn more httpsgithub.comtesting-libraryjest-dom
+import '@testing-libraryjest-dom';
