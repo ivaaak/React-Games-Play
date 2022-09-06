b39d9f9d47a77625aa57aa29ea87ba427ab14ddd
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-09-06 13:22:28 +0300

    Added Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Header from './components/Header';
 import WelcomeWorld from './components/WelcomeWorld';
+import GameCatalog from './components/GameCatalog';
+import CreateGame from './components/CreateGame';
 
 function App() {
+    const routes = {
+        '/home': WelcomeWorld,
+        '/games': GameCatalog,
+        '/create-game':CreateGame,
+    };
+
     return (
         <div id="box">
-            
-		<Header />
-
+            <Header />
             <main id="main-content">
                 <WelcomeWorld />
             </main>
-
         </div>
     );
 }
+export default App;
