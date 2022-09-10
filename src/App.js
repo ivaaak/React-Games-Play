641f60a80999bb88f431c72279f759ac8bb0508b
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-09-10 18:32:16 +0300

    Added details page to App/Router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import { useState, createElement } from 'react';
-
 import Header from './components/Header';
 import WelcomeWorld from './components/WelcomeWorld';
-import GameCatalog from './components/GameCatalog';
+import GameCatalog from './components/GameCatalog/GameCatalog';
 import CreateGame from './components/CreateGame';
 import Login from './components/Login';
 import Register from './components/Register';
 import ErrorPage from './components/ErrorPage';
+import GameDetails from './components/GameDetails';
 
 function App() {
     const [page, setPage] = useState('/home');
 
-    const routes = {
-        '/home': <WelcomeWorld />,
-        '/games': <GameCatalog />,
-        '/create-game': <CreateGame />,
-        '/login': <Login />,
-        '/register': <Register />,
-    };
-
     const navigationChangeHandler = (path) => {
         setPage(path);
     }
+
+    const router = (path) => {
+        let pathNames = path.split('/');
+
+        let rootPath = pathNames[1];
+        let argument = pathNames[2];
+
+        const routes = {
+            'home': <WelcomeWorld />,
+            'games': <GameCatalog navigationChangeHandler={navigationChangeHandler} />,
+            'create-game': <CreateGame />,
+            'login': <Login />,
+            'register': <Register />,
+            'details': <GameDetails id={argument} />
+        };
+
+        return routes[rootPath]
+    }
+
     return (
         <div id="box">
             <Header 
@@ -29,13 +40,10 @@ function App() {
             />
 
             <main id="main-content">
-                { 
-                    routes[page] || <ErrorPage />
-                } 
+                { router(page) || <ErrorPage />} 
             </main>
 
         </div>
     );
 }
-
 export default App;
