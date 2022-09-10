043c0b9b4b5226acb2b2b5b5662c72200328bae4
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-09-10 18:31:10 +0300

    Service Import, moved fetch out of catalog comp

--- src/components/GameCatalog.js
+++ src/components/GameCatalog.js
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react';
 import GameCard from './GameCard';
+import * as gameService from '../../services/gameService';
 
 const GameCatalog = () => {
     const [games, setGames] = useState([]);
-    
+
     useEffect(() => {
         setTimeout(() => {
-            fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-                .then(res => res.json())
+            gameService.getAll()
                 .then(result => {
                     setGames(result);
-                });
+                })
         }, 1000);
     }, []);
 
@@ -24,5 +24,4 @@ const GameCatalog = () => {
         </section>
     );
 }
-
 export default GameCatalog;
