ae1e5f06c29b7e93c54f2a866b6def040646ea32
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-09-11 08:01:46 +0300

    Added navigation to change catalog

--- src/components/GameCatalog.js
+++ src/components/GameCatalog.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
-import GameCard from './GameCard';
 import * as gameService from '../../services/gameService';
+import GameCard from './GameCard';
 
-const GameCatalog = () => {
+const GameCatalog = ({
+    navigationChangeHandler
+}) => {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
@@ -13,15 +15,16 @@ const GameCatalog = () => {
                 })
         }, 1000);
     }, []);
-
+    
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
             { games.length > 0 
-                ? games.map(x => <GameCard key={x._id} game={x} />)
+                ? games.map(x => <GameCard key={x._id} game={x} navigationChangeHandler={navigationChangeHandler} />)
                 : <h3 className="no-articles">No games yet</h3>
             }
         </section>
     );
 }
+
 export default GameCatalog;
