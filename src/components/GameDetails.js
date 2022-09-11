ed2baee8d9c48891710a4277a432974d86ff931f
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-09-11 08:03:38 +0300

    Added state, get game from service.

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,21 +1,32 @@
-const GameDetails = () => {
+import { useState, useEffect } from 'react';
+
+import * as gameService from '../services/gameService';
+
+const GameDetails = ({
+    id
+}) => {
+    const [game, setGame] = useState({});
+
+    useEffect(async () => {
+        let result = await gameService.getOne(id);
+
+        setGame(result);
+    }, []);
+
     return (
         <section id="game-details">
             <h1>Game Details</h1>
             <div className="info-section">
 
                 <div className="game-header">
-                    <img className="game-img" src="images/MineCraft.png" />
-                    <h1>Bright</h1>
-                    <span className="levels">MaxLevel: 4</span>
-                    <p className="type">Action, Crime, Fantasy</p>
+                    <img className="game-img" src={game.imageUrl} />
+                    <h1>{game.title}</h1>
+                    <span className="levels">MaxLevel: {game.maxLevel}</span>
+                    <p className="type">{game.category}</p>
                 </div>
 
                 <p className="text">
-                    Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                    with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                    creatures live side by side with humans. A human cop is forced
-                    to work with an Orc to find a weapon everyone is prepared to kill for.
+                    {game.summary}
                 </p>
 
                 <div className="details-comments">
@@ -30,13 +41,11 @@ const GameDetails = () => {
                     </ul>
                     <p className="no-comment">No comments.</p>
                 </div>
-
                 <div className="buttons">
                     <a href="#" className="button">Edit</a>
                     <a href="#" className="button">Delete</a>
                 </div>
             </div>
-
             <article className="create-comment">
                 <label>Add new comment:</label>
                 <form className="form">
@@ -44,7 +53,6 @@ const GameDetails = () => {
                     <input className="btn submit" type="submit" value="Add Comment" />
                 </form>
             </article>
-
         </section>
     );
 }
